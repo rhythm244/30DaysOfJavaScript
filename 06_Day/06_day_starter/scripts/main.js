af59880a88300fe0e08b217b5686bb488079ee29
rhythm244 <63957603+rhythm244@users.noreply.github.com>
2020-04-26 21:48:39 +0700

    adding variable before go to bed
    try to do challenge

--- 06_Day/06_day_starter/scripts/main.js
+++ 06_Day/06_day_starter/scripts/main.js
@@ -2,3 +2,34 @@ console.log(countries)
 alert('Open the console and check if the countries has been loaded')
 
 console.log("thong thong 555")
+//homework day6
+
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+  ]
+  
+  const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB'
+  ]
+  
+  const mernStack = ['MongoDB', 'Express', 'React', 'Node']
+  
+  //1. Iterate 0 to 10 using for loop, do the same using while and do while loop
+
+  
