b690f920bb87d654176b2c725b157af3bcd89517
rhythm244 <63957603+rhythm244@users.noreply.github.com>
2020-04-27 14:32:12 +0700

    hw 1-3

--- 06_Day/06_day_starter/scripts/main.js
+++ 06_Day/06_day_starter/scripts/main.js
@@ -1,10 +1,10 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+
+console.log('Open the console and check if the countries has been loaded')
 
 console.log("thong thong 555")
 //homework day6
 
-const countries = [
+const countries2 = [
     'Albania',
     'Bolivia',
     'Canada',
@@ -31,5 +31,19 @@ const countries = [
   const mernStack = ['MongoDB', 'Express', 'React', 'Node']
   
   //1. Iterate 0 to 10 using for loop, do the same using while and do while loop
+/*
+  for(var i =0;i<10;i++){
+    console.log(i+1);
+  }
+*/
+  //2. Iterate 10 to 0 using for loop, do the same using while and do while loop
 
-  
+  for(var j=10;j>-1;j--){
+    console.log(j);
+  }
+
+  //3. Iterate 0 to n using for loop
+  let n=20;
+  for(var k=0;k<n;k++){
+    console.log(k)
+  }
