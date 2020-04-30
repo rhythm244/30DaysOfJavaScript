c1c7ba8c8c83e70ea42b4f4becfee71d9807d2d5
rhythm244 <63957603+rhythm244@users.noreply.github.com>
2020-04-30 16:10:57 +0700

    to find arr length

--- 06_Day/06_day_starter/scripts/main.js
+++ 06_Day/06_day_starter/scripts/main.js
@@ -230,31 +230,140 @@ for (var b = 0; b < 5; b++) {
 console.log(randomArr)
 */
 console.log("--------------------------")
-/*
+
 //14. Develop a small script which generate array of 5 random numbers and the numbers must be unique
 var randomArr = []
 var random1 = 0
 var count = 0
 
-for (var b = 0; (b <= count) && (count<5) ; b++) 
-{
+for (var b = 0; (b <= count) && (count < 5); b++) {
   console.log(randomArr.length)
-  
-  for (var r = 0; r <= b; r++)
-  {
-    random1 = Math.round(Math.random()*10)
-    if (!(randomArr.includes(random1))) 
-    {
+
+  for (var r = 0; r <= b; r++) {
+    random1 = Math.round(Math.random() * 100)
+    if (!(randomArr.includes(random1))) {
       randomArr.push(random1)
       count = randomArr.length
+      break
     }
-    
+
   }
 }
 console.log(randomArr)
 
-
+//test for in
 for (var oo in randomArr) {
   console.log(oo)
 }
+/*
+15. Develop a small script which generate a six characters random id:
+
+    ```sh
+    5j2khz
 */
+//copy form 3KINGZ solutions
+let chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+let randomChars = ""
+for (let i = 0; i < 6; i++) {
+  let random = Math.floor(Math.random() * chars.length);
+  randomChars += chars[random];
+}
+console.log(randomChars)
+
+let randomUniqueArr = [];
+for (let i = 5; randomUniqueArr.length < i;) {
+  let random = Math.floor(Math.random() * 10);
+  if (randomUniqueArr.indexOf(random) === -1) {
+    randomUniqueArr.push(random);
+  }
+}
+console.log(randomUniqueArr)
+
+/*1. Develop a small script which generate any number of characters random id:
+
+```sh
+fe3jo1gl124g
+```
+
+```sh
+xkqci4utda1lmbelpkm03rba
+```
+*/
+let randomChars2 = ""
+var inputUser = 20
+for (let i = 0; i < inputUser; i++) {
+  let random = Math.floor(Math.random() * chars.length);
+  randomChars2 += chars[random];
+
+}
+console.log(randomChars2)
+
+/*1. Write a script which generates a random hexadecimal number.
+
+    ```sh
+    '#ee33df'
+    ```
+hexString = yourNumber.toString(16);
+*/
+let myNumber = Math.floor(Math.random() * 25000)
+var hexString = myNumber.toString(16)
+console.log(hexString + "," + "decimal is " + myNumber)
+
+/*
+Colors or RGB
+Colors in a computer program are represented by combining 3 "pigments". These pigments are Red, Green, and Blue (which contrasts with the "primary" colors we are used to as a child). By combining some amount of Red, some amount of Green, and some amount of Blue, any (displayable) color can be achieved.
+
+RGB Colors
+All colors on a computer are made up by combining the light from three colors (red, blue, and green). Black is [0,0,0], and White is [255, 255, 255]; Gray is any [x,x,x] where all the numbers are the same.
+
+The max value of each of the colors is 255. The minimum value is 0.
+
+Colors are almost always written with the Red value first, the Green value second, and the Blue value third. Memorize "RGB" and you will remember the ordering.
+
+Here are some examples:
+
+White = [ 255, 255, 255 ]
+Black = [ 0, 0, 0 ]
+A "perfect" Blue = [0,0,255]
+A "prefect" Red = [255, 0, 0]
+A "middle" Gray = [ 122, 122, 122]
+
+1. Write a script which generates a random rgb color number.
+
+    ```sh
+    rgb(240,180,80) Math.floor(Math.random()*250)
+    ```
+*/
+let randomRgb = []
+for (let v = 0; v < 3; v++) {
+  randomRgb[v] = Math.floor(Math.random() * 250)
+}
+console.log("RGB = " + randomRgb[0] + " " + randomRgb[1] + " " + randomRgb[2])
+
+/*1. Using the above countries array, create the following new array.
+
+    ```sh
+    ["ALBANIA", "BOLIVIA", "CANADA", "DENMARK", "ETHIOPIA", "FINLAND", "GERMANY", "HUNGARY", "IRELAND", "JAPAN", "KENYA"]
+    ```
+*/
+
+for (var ooo in countries2) {
+  countries2[ooo] = countries2[ooo].toUpperCase()
+}
+console.log(countries2)
+
+/*1. Using the above countries array, create an array for countries length'.
+
+    ```sh
+    [7, 7, 6, 7, 8, 7, 7, 7, 7, 5, 5]
+*/
+var len = []
+for (let ccc = 0; ccc < countries2.length ; ccc++){
+  len.push(countries2[ccc].length)
+}
+console.log(len)
+console.log("---------------------------")
+
+
+
+
